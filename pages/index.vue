85ef3779a0db1f7be2757333f85ffb4e3834ffe3
paulopinhobolder <paulo.pinho@bolder.pt>
2019-02-25 00:59:09 +0000

    More pages and scss files

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,38 @@
 <template>
   <section class="container bg-primary">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxty
-      </h1>
-      <h2 class="subtitle">
-        My Vue project based on Nuxt.js for Blip job apply
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+    <div class="row">
+      <div class="col-12">
+        <logo />
       </div>
+      <div class="col-12">
+        <h1 class="title">
+          nuxty
+        </h1>
+        <h2 class="subtitle">
+          My Vue project based on Nuxt.js for Blip job apply
+        </h2>
+        <div class="links">
+          <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+          <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        </div>
+      </div>
+
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+  import Logo from '~/components/Logo.vue'
 
-export default {
-  components: {
-    Logo
+  export default {
+    components: {
+      Logo
+    }
   }
-}
 
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="scss">
+  @import "./assets/scss/pages/_index.scss";
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
---
+++ pages/search.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+    @import "./assets/scss/pages/_search.scss";
+</style>
